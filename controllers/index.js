0ba453076946da2372d0bf62fca77c3661a5b848
VictorMaVargas <victormavargas@gmail.com>
2021-06-02 19:33:03 +0200

    estructura básica

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require ("express").Router();
+const pages = require ("./pages")
+
+router.get('/', pages.home)
+router.get('/film/:title', pages.film)
+
+router.post('/film', pages.form)
+
+module.exports = router
---
+++ controllers/pages.js
@@ -0,0 +1,34 @@
+const {apiKeyConfig} = require ("../config");
+//const {apiKey} = require ("../config");
+//import {apiKeyConfig} from "./config.js"; 
+const fetch = require("node-fetch");
+
+const pages = {
+  home: (req, res) => {
+    let datos = {
+      título: "película",
+    };
+    res.status(200).render("home", datos);
+  },
+  film: (req, res) => {
+    let titulo = req.params.title;
+    fetch(`http://www.omdbapi.com/?t=${titulo}&apikey=${apiKeyConfig.apiKey}`)
+    .then(res=>res.json())
+    .then(data => {
+        //console.log(data);
+        res.status(200).render("film", {'data':data})
+    })
+    .catch (e => console.error(e))
+    //console.log(response);
+    
+  },
+  form: (req, res) => {
+    console.log(req.body.titulo);
+    const url = `film/${req.body.titulo}`
+    console.log(url);
+    res.redirect(url);
+  },
+};
+module.exports = pages;
+
+//(`http://www.omdbapi.com/?t=${titulo}&apikey=3265fadd`)
